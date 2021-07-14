09ab28d059005d4c49d297177f30e8e10361fdad
nher1625 <nher1625@gmail.com>
2021-07-14 15:19:05 -0400

    First commit...

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-12">
+        <Table
+          :name="`${resource} table`"
+          :items="items"
+          :fields="fields"
+          :totalPages="totalPages"
+          :currentPage="page"
+          :per-page="perPage"
+          @page-changed="pageChanged"
+          @sort-items="sort"
+          @limit-changed="limitChanged"
+        />
+      </div>
+    </div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Table from '@/components/table/TableBase'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Table
+  },
+  data () {
+    return {
+      items: [],
+      fields: [
+        { key: 'id', label: 'ID', sortable: true },
+        { key: 'first_name', label: 'Name' },
+        { key: 'year', label: 'Year', sortable: true },
+        { key: 'color', label: 'Color' },
+        { key: 'created_at', label: 'Created At' }
+      ],
+      totalPages: null,
+      resource: 'post',
+      page: 1
+    }
+  },
+  methods: {
+    fetchData (resource = 'posts', page = 1, perPage = 10) {
+      return fetch(`https://reqres.in/api/${resource}?page=${page}&per_page=${perPage}`)
+        .then((response) => {
+          return response.json()
+        })        
+    },
+    async refreshTable (resource, page, perPage) {
+      try {
+        let res = await this.fetchData(resource, page, perPage)
+        this.items = res.data
+        this.totalPages = res.total_pages
+        this.page = res.page
+      } catch (e) {
+        window.console.log(e)
+      }
+    },
+    pageChanged (page) {
+      this.refreshTable(this.resource, page, this.perPage)
+    },
+    sort (items) {
+      this.items = items
+    },
+    limitChanged (limit) {
+      this.page = 1
+      this.perPage = limit
+      this.refreshTable(this.resource, this.page, limit)
+    }
+  },
+  async mounted () {
+    this.refreshTable(this.resource, this.page, this.perPage)
   }
 }
 </script>
@@ -21,7 +85,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/table/TableBase.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container table-container">
+    <div class="row" v-if="name">
+      <div class="col">
+        <h3 class="text-capitalize">{{name}}</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="row toolbar mb-2">
+        <div class="col"></div>
+        <div class="col-2">
+          <select v-model="limit" class="form-select" aria-label="size 3 select example">
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+          </select>
+        </div>
+      </div>
+      <div class="col">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col" v-for="field in fields" :key="field.key">
+                {{ field.label || field.key }}
+                <a v-if="field.sortable" class="p-1" style="cursor:pointer" @click.prevent="sortItemsByProperty(field.key)">
+                  <i class="fas fa-sort"></i>
+                </a>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in items" :key="item.id">
+              <td v-for="itemField in item" :key="itemField">{{ itemField }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li :class="page - 1 > 0 ? 'page-item' : 'page-item disabled'">
+              <a class="page-link" href="#" @click.prevent="changePage(page - 1)">Previous</a>
+            </li>
+            <li class="page-item" v-for="num in totalPages" :key="num">
+              <a :class="page == num ? 'page-link bg-primary text-white' : 'page-link'" href="#" @click.prevent="changePage(num)">{{ num }}</a>
+            </li>
+            <li :class="page + 1 <= totalPages ? 'page-item' : 'page-item disabled'">
+              <a class="page-link" href="#" @click.prevent="changePage(page + 1)">Next</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from 'underscore'
+export default {
+  props: {
+    name: String,
+    items: Array,
+    fields: Array,
+    totalPages: Number,
+    currentPage: Number,
+    perPage: Number
+  },
+  data () {
+    return {
+      sortDirection: 'dsc',
+      page: this.$props.currentPage || 1,
+      limit: this.$props.perPage || 10
+    }
+  },
+  methods: {
+    changePage(n) {
+      if (this.page >= 1 && n <= this.totalPages) {
+        this.page = n
+        this.$emit('page-changed', n)
+      }
+    },
+    sortItemsByProperty(property) {
+      const items = [...this.items]
+      const sortedItems = _.sortBy(items, property)
+      // window.console.log(sortedItems, property)
+      if (this.sortDirection === 'asc') {
+        this.$emit('sort-items', sortedItems)
+        this.sortDirection = 'dsc'
+      } else if (this.sortDirection === 'dsc') {
+        this.$emit('sort-items', sortedItems.reverse())
+        this.sortDirection = 'asc'
+      }
+    }
+  },
+  watch: {
+    limit (val) {
+      this.page = 1
+      this.$emit('limit-changed', val)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
